f9b7c36ce0d1f2d1aec4d6731472978fc09a5852
maxim-hj <maxi3by@gmail.com>
2022-05-26 20:41:38 +0300

    Updates

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 (()=>{var __webpack_modules__={"./src/js/main.js":
 /*!************************!*\
   !*** ./src/js/main.js ***!
-  \************************/()=>{eval("\n\n//# sourceURL=webpack://mytemplate/./src/js/main.js?")}},__webpack_exports__={};__webpack_modules__["./src/js/main.js"]()})();
+  \************************/()=>{eval("var btnAuth = document.querySelector('.auth__btn');\nvar modal = document.querySelector('.modal');\nvar btnAuthClose = document.querySelector('.modal__close');\nbtnAuth.addEventListener('click', function () {\n  modal.classList.remove('hidden');\n});\nbtnAuthClose.addEventListener('click', function () {\n  modal.classList.add('hidden');\n});\n\n//# sourceURL=webpack://mytemplate/./src/js/main.js?")}},__webpack_exports__={};__webpack_modules__["./src/js/main.js"]()})();
 //# sourceMappingURL=main.js.map
