4ed87ba0bb6b4b461f60d3b3bd365c82457a924f
maxim-hj <maxi3by@gmail.com>
2022-05-26 20:23:09 +0300

    Updates

---
+++ js/main.js
@@ -0,0 +1,5 @@
+(()=>{var __webpack_modules__={"./src/js/main.js":
+/*!************************!*\
+  !*** ./src/js/main.js ***!
+  \************************/()=>{eval("\n\n//# sourceURL=webpack://mytemplate/./src/js/main.js?")}},__webpack_exports__={};__webpack_modules__["./src/js/main.js"]()})();
+//# sourceMappingURL=main.js.map
